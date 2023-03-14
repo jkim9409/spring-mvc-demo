3e11bf2046337021f97e6840b6db01e50bd7c1f6
jkim9409 <jkimnyc2020@gmail.com>
2023-03-14 20:04:09 +0900

    dealing with view with various way
    dealing with view with various way

--- springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
+++ springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -2,6 +2,7 @@ package hello.springmvc.basic.response;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,8 +11,24 @@ public class ResponseViewController {
 
     @RequestMapping("/response-view-v1")
     public ModelAndView responseViewV1(){
-        ModelAndView mav = new ModelAndView("hello").addObject("data", "hello");
+        ModelAndView mav = new ModelAndView("response/hello").addObject("data", "hello");
 
         return mav;
+    }
+
+    //Another way,
+    //Returning String value, (using addAttribute)
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model){
+        model.addAttribute("data", "hello!");
+        return "response/hello";
+    }
+
+    //Don't recommend this method. but still work.
+    //When Controller path and Views name matches
+    //Note that we don't have @ResponseBody here.
+    @RequestMapping("/response/hello")
+    public void responseViewV3(Model model){
+        model.addAttribute("data", "hello!");
     }
 }
